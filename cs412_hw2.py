a481441fdef67aed41b88bc600e21a1d24539ff9
bcgood98 <bengood98@gmail.com>
2021-03-07 16:48:58 -0900

    Added a little bit more

--- cs412_hw2.py
+++ cs412_hw2.py
@@ -10,3 +10,12 @@ def recombination(parent_pool):
 	child = [random.choice([parents[0][i], parents[1][i]]) for i in range(genolen)]
 	
 	return child
+
+# Global recombination
+def globalrec(pool, np, no):
+	pass
+
+# Fitness function
+def eval(id):
+	val = 21.5 + id[0] * math.sin(4 * math.pi * id[0]) + id[1] * math.sin(20 * math.pi * id[1])
+	return val
